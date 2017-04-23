0bb518fe81bb0f666049115c19b7a43817653d43
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-04-23 19:30:16 -0400

    added area

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function CalculateArea(){
   var radius =document.getElementById('myButton').value;
 
   var fineCircle=document.createElement('div');
-  fineCircle.style.width=radius* 2 + 'px' ;
+  fineCircle.style.width=radius * 2 + 'px' ;
   fineCircle.style.height=radius * 2 + 'px' ;
   fineCircle.style.backgroundImage="url('images/haramBAE.jpg')";
   fineCircle.style.borderRadius='50%';
   document.body.appendChild(fineCircle);
 
-  var yourRadius = radius*radius;
-  document.body.appendChild("The radius of your circle is " +yourRadius);
+  var yourArea = 3.14 * radius * radius;
 
+  document.getElementById('area').innerHTML = yourArea;
+
+console.log('hi' + yourArea);
 }
 
   // var radius=document.getElementById(fineCircle);
   // document.write('<p>The area of the circle is '+(radius* 2 * Math.PI)+'.</p>')
-
-console.log('hi');
